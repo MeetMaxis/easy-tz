fdf5d43a1cc36d10a462e7ceb95ea1cb53545ba5
Kirilldorkin <kirilldorkin500@gmail.com>
2023-09-23 17:02:49 +0600

    third

--- script.js
+++ script.js
@@ -1,59 +1,50 @@
-const typeAdress = document.getElementById("typeAdress");
-const sendEtherButton = document.getElementById("sendEtherButton");
-const continueButton = document.getElementById("continueButton");
-const statusMessage = document.getElementById("statusMessage");
+
+const typeAdress = document.getElementById('typeAdress');
+const sendEtherButton = document.getElementById('sendEtherButton');
+const statusMessage = document.getElementById('statusMessage');
 
 let validationComplete = false;
 
-typeAdress.addEventListener("input", (e) => {
-  const value = e.target.value;
-
-  // Если нет, то добавляем "0x" в начало
-  if (!/^0x/.test(value)) {
-    e.target.value = "0x" + value;
-  }
-
-  // Удаляем кириллицу
-  const newValue = value.replace(/[а-яА-Я]/g, "");
-  e.target.value = newValue;
-
-  // Проверка на наличие только латинских букв, цифр и специальных символов
-  if (
-    /^[a-zA-Z0-9!@#$%^&*()-_+=<>?:"',./\[\]{}|\\]+$/g.test(newValue) &&
-    newValue.length >= 20
-  ) {
-    sendEtherButton.disabled = false;
-  } else {
-    sendEtherButton.disabled = true;
-  }
-});
-
-sendEtherButton.addEventListener("click", async () => {
+sendEtherButton.addEventListener('click', async () => {
   try {
     const value = typeAdress.value;
 
-    // Проверка на наличие только латинских букв, цифр и специальных символов и длину
-    if (
-      /^[a-zA-Z0-9!@#$%^&*()-_+=<>?:"',./\[\]{}|\\]+$/g.test(value) &&
-      value.length >= 20
-    ) {
-      statusMessage.textContent = "Ожидание...";
+    if (!value) {
+      alert('Поле не заполнено, повторите попытку!');
+      return;
+    }
 
-      // Задержка в 10 секунд
-      setTimeout(() => {
-        statusMessage.textContent = "Успешно";
-        typeAdress.disabled = true; // Блокировка поля ввода фразы
-        sendEtherButton.disabled = true; // Отключаем кнопку "Подтвердить"
-        continueButton.disabled = false; // Включение кнопки "Продолжить"
-      }, 10000);
+    if (validationComplete) {
+      typeAdress.value = ''; // Очищаем поле ввода
+      sendEtherButton.textContent = 'Подтвердить'; // Меняем текст кнопки
+      validationComplete = false; // Помечаем, что проверка не выполнена
+      statusMessage.textContent = ''; // Очищаем статусное сообщение
     } else {
-      alert("Поле не заполнено правильно, повторите попытку!");
+      // Проверка на наличие только латинских букв, цифр и специальных символов и длину
+      if (/^[a-zA-Z0-9!@#$%^&*()-_+=<>?:"',./\[\]{}|\\]+$/g.test(value) && value.length >= 20) {
+        statusMessage.textContent = 'Ожидание...';
+        typeAdress.disabled = true; // Блокировка поля ввода фразы
+        sendEtherButton.disabled = true; // Отключаем кнопку
+
+        // Задержка в 10 секунд
+        setTimeout(() => {
+          validationComplete = true; // Помечаем, что проверка выполнена успешно
+          statusMessage.textContent = '';
+          typeAdress.disabled = false; // Разблокировка поля ввода фразы
+          sendEtherButton.textContent = 'Продолжить'; // Меняем текст кнопки
+          sendEtherButton.disabled = false; // Включаем кнопку
+        }, 10000);
+      } else {
+        alert('Поле не заполнено правильно, повторите попытку!');
+      }
     }
   } catch (error) {
     console.error(error);
   }
 });
 
-continueButton.addEventListener("click", () => {
-  sendEtherButton.click(); // Вызываем клик по кнопке "Подтвердить" после нажатия "Продолжить"
+sendEtherButton.addEventListener('click', () => {
+  if (validationComplete) {
+    typeAdress.value = ''; // Очищаем поле ввода
+  }
 });
