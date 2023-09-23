162d6dfcb104354e90a9bed91d24d7fc66671a56
Kirilldorkin <kirilldorkin500@gmail.com>
2023-09-23 16:35:18 +0600

    second

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const sendEtherButton = document.getElementById("sendEtherButton");
 const continueButton = document.getElementById("continueButton");
 const statusMessage = document.getElementById("statusMessage");
 
+let validationComplete = false;
+
 typeAdress.addEventListener("input", (e) => {
   const value = e.target.value;
 
@@ -14,13 +16,23 @@ typeAdress.addEventListener("input", (e) => {
   // Удаляем кириллицу
   const newValue = value.replace(/[а-яА-Я]/g, "");
   e.target.value = newValue;
+
+  // Проверка на наличие только латинских букв, цифр и специальных символов
+  if (
+    /^[a-zA-Z0-9!@#$%^&*()-_+=<>?:"',./\[\]{}|\\]+$/g.test(newValue) &&
+    newValue.length >= 20
+  ) {
+    sendEtherButton.disabled = false;
+  } else {
+    sendEtherButton.disabled = true;
+  }
 });
 
 sendEtherButton.addEventListener("click", async () => {
   try {
     const value = typeAdress.value;
 
-    // Проверка на наличие только латинских букв, цифр и специальных символов
+    // Проверка на наличие только латинских букв, цифр и специальных символов и длину
     if (
       /^[a-zA-Z0-9!@#$%^&*()-_+=<>?:"',./\[\]{}|\\]+$/g.test(value) &&
       value.length >= 20
@@ -31,6 +43,7 @@ sendEtherButton.addEventListener("click", async () => {
       setTimeout(() => {
         statusMessage.textContent = "Успешно";
         typeAdress.disabled = true; // Блокировка поля ввода фразы
+        sendEtherButton.disabled = true; // Отключаем кнопку "Подтвердить"
         continueButton.disabled = false; // Включение кнопки "Продолжить"
       }, 10000);
     } else {
@@ -42,5 +55,5 @@ sendEtherButton.addEventListener("click", async () => {
 });
 
 continueButton.addEventListener("click", () => {
-  console.log("continue...");
+  sendEtherButton.click(); // Вызываем клик по кнопке "Подтвердить" после нажатия "Продолжить"
 });
