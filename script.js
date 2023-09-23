a8e003a66f6462a3f74bdcb9d5e9a6ee401b62d9
Kirilldorkin <kirilldorkin500@gmail.com>
2023-09-23 18:31:05 +0600

    fourth

--- script.js
+++ script.js
@@ -1,50 +1,52 @@
+const typeAdress = document.getElementById("typeAdress");
+const sendEtherButton = document.getElementById("sendEtherButton");
+const statusMessage = document.getElementById("statusMessage");
+const continueButton = document.getElementById("continueButton");
 
-const typeAdress = document.getElementById('typeAdress');
-const sendEtherButton = document.getElementById('sendEtherButton');
-const statusMessage = document.getElementById('statusMessage');
+let isValidationComplete = false;
+continueButton.style.display = "none";
 
-let validationComplete = false;
+typeAdress.addEventListener("input", (e) => {
+  const value = e.target.value;
 
-sendEtherButton.addEventListener('click', async () => {
+  if (!/^0x/.test(value)) {
+    e.target.value = "0x" + value;
+  }
+
+  const newValue = value.replace(/[а-яА-Я]/g, "");
+  e.target.value = newValue;
+});
+
+sendEtherButton.addEventListener("click", async () => {
   try {
     const value = typeAdress.value;
 
-    if (!value) {
-      alert('Поле не заполнено, повторите попытку!');
-      return;
-    }
+    if (
+      /^[a-zA-Z0-9!@#$%^&*()-_+=<>?:"',./\[\]{}|\\]+$/g.test(value) &&
+      value.length >= 20
+    ) {
+      statusMessage.textContent = "Ожидание...";
+      sendEtherButton.disabled = true; // Делаем кнопку sendEtherButton неактивной
+      typeAdress.disabled = true;
 
-    if (validationComplete) {
-      typeAdress.value = ''; // Очищаем поле ввода
-      sendEtherButton.textContent = 'Подтвердить'; // Меняем текст кнопки
-      validationComplete = false; // Помечаем, что проверка не выполнена
-      statusMessage.textContent = ''; // Очищаем статусное сообщение
+      setTimeout(() => {
+        statusMessage.textContent = "Успешно";
+        
+        isValidationComplete = true;
+        continueButton.style.display = "block"; // Отображаем кнопку continueButton
+      }, 10000);
     } else {
-      // Проверка на наличие только латинских букв, цифр и специальных символов и длину
-      if (/^[a-zA-Z0-9!@#$%^&*()-_+=<>?:"',./\[\]{}|\\]+$/g.test(value) && value.length >= 20) {
-        statusMessage.textContent = 'Ожидание...';
-        typeAdress.disabled = true; // Блокировка поля ввода фразы
-        sendEtherButton.disabled = true; // Отключаем кнопку
-
-        // Задержка в 10 секунд
-        setTimeout(() => {
-          validationComplete = true; // Помечаем, что проверка выполнена успешно
-          statusMessage.textContent = '';
-          typeAdress.disabled = false; // Разблокировка поля ввода фразы
-          sendEtherButton.textContent = 'Продолжить'; // Меняем текст кнопки
-          sendEtherButton.disabled = false; // Включаем кнопку
-        }, 10000);
-      } else {
-        alert('Поле не заполнено правильно, повторите попытку!');
-      }
+      alert("Поле не заполнено правильно, повторите попытку!");
     }
   } catch (error) {
     console.error(error);
   }
 });
 
-sendEtherButton.addEventListener('click', () => {
-  if (validationComplete) {
-    typeAdress.value = ''; // Очищаем поле ввода
-  }
+continueButton.addEventListener("click", () => {
+  continueButton.style.display = "none"; // Скрываем кнопку continueButton
+  sendEtherButton.disabled = false;
+  typeAdress.disabled = false;
+  typeAdress.value = ""; // Очищаем поле ввода
+  statusMessage.textContent = "";
 });
